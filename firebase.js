ce361e3c960efb8961f8a6233b2169aaf4302d05
ErwanLeBras12 <113606658+ErwanLeBras12@users.noreply.github.com>
2023-10-22 18:16:00 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
      
   // Configuration Firebase (à obtenir à partir de votre projet Firebase)
   var firebaseConfig = {
-    apiKey: "AIzaSyAJ7QUcvK4F2n2qeWMLviBXNIJzM45kNXw",
-    databaseURL: "https://odoor-99a82-default-rtdb.europe-west1.firebasedatabase.app/",    
-    projectId: "odoor-99a82",
-    storageBucket: "odoor-99a82.appspot.com",
-    appId: "1:349207637584:android:7670107a1f508fecd2eb55"  
+      apiKey: "AIzaSyCKxxd4I-R08z6gWzTzMLkYX-T6RGcZpGo",
+      authDomain: "odoor-99a82.firebaseapp.com",
+      databaseURL: "https://odoor-99a82-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "odoor-99a82",
+      storageBucket: "odoor-99a82.appspot.com",
+      messagingSenderId: "349207637584",
+      appId: "1:349207637584:web:6ea9efeb4f2189ebd2eb55",
+      measurementId: "G-6Q46YKPTSN"  
   };
 
   // Initialisez Firebase avec la configuration
@@ -16,14 +19,14 @@
       
    
   // Obtenez une référence à la base de données
- // var database = firebase.database();
+  var database = firebase.database();
   
   // Obtenez une référence à un chemin spécifique dans la base de données
- // var dataRef = database.ref("status");
+  var dataRef = database.ref("status");
 
   // Écoutez les modifications des données
- // dataRef.on("value", function(snapshot) {
+  dataRef.on("value", function(snapshot) {
     // La fonction callback sera appelée chaque fois que les données changent
-  //  var data = snapshot.val(); // Les données sont dans l'objet snapshot.val()
- //   console.log(data); // Affichez les données dans la console
-//    });
+   var data = snapshot.val(); // Les données sont dans l'objet snapshot.val()
+    console.log(data); // Affichez les données dans la console
+    });

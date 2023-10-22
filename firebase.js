6e9e635d0a9653014a1cdbe605e846d3bb642e03
ErwanLeBras12 <113606658+ErwanLeBras12@users.noreply.github.com>
2023-10-22 21:11:27 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,12 +9,29 @@
        var database = firebaseApp.database();
        const auth = firebaseApp.auth();
         console.log("test");
-      // Obtenez une référence à un chemin spécifique dans la base de données
-      var dataRef = database.ref("status");
-    
-      // Écoutez les modifications des données
-       dataRef.on("value", function(snapshot) {
-        // La fonction callback sera appelée chaque fois que les données changent
-       var data = snapshot.val(); // Les données sont dans l'objet snapshot.val()
-        console.log(data); // Affichez les données dans la console
+      //Obtenez une référence à un chemin spécifique dans la base de données
+        var dataRef = database.ref("status");
+        
+        // Obtenez la valeur initiale de "status" (une seule fois)
+        dataRef.once("value", function(snapshot) {
+            var initialStatus = snapshot.val();
+            console.log("Valeur initiale de status : " + initialStatus);
+        });
+        
+        // Maintenant, ajoutez un écouteur pour les modifications de "status"
+        dataRef.on("value", function(snapshot) {
+            // La fonction callback sera appelée chaque fois que "status" change
+            var data = snapshot.val();
+            console.log("Nouvelle valeur de status : " + data);
+        });
+        
+        // Répétez le même processus pour "battery" si nécessaire
+        var batteryRef = database.ref("battery");
+        batteryRef.once("value", function(snapshot) {
+            var initialBattery = snapshot.val();
+            console.log("Valeur initiale de battery : " + initialBattery);
+        });
+        batteryRef.on("value", function(snapshot) {
+            var data = snapshot.val();
+            console.log("Nouvelle valeur de battery : " + data);
         });
